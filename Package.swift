e94c69e1d4c03df8976351355f1781156e00ff36
p-x9 <50244599+p-x9@users.noreply.github.com>
2023-06-25 22:59:29 +0900

    add dependencies

--- Package.swift
+++ Package.swift
@@ -1,23 +1,29 @@
 // swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftSyntaxPlus",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftSyntaxPlus",
-            targets: ["SwiftSyntaxPlus"]),
+            targets: ["SwiftSyntaxPlus"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-syntax.git",
+                 from: "509.0.0-swift-DEVELOPMENT-SNAPSHOT-2023-06-05-a"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SwiftSyntaxPlus"),
+            name: "SwiftSyntaxPlus",
+            dependencies: [
+                .product(name: "SwiftSyntax", package: "swift-syntax")
+            ]
+        ),
         .testTarget(
             name: "SwiftSyntaxPlusTests",
-            dependencies: ["SwiftSyntaxPlus"]),
+            dependencies: ["SwiftSyntaxPlus"]
+        ),
     ]
 )
