1e71b1e913ec0c451fef5a891397d2993e9bfd83
p-x9 <50244599+p-x9@users.noreply.github.com>
2023-06-26 09:15:28 +0900

    make public

--- Sources/SwiftSyntaxPlus/PatternBindingSyntax+.swift
+++ Sources/SwiftSyntaxPlus/PatternBindingSyntax+.swift
@@ -11,7 +11,7 @@ import SwiftSyntax
 import SwiftSyntaxBuilder
 
 extension PatternBindingSyntax {
-    var setter: AccessorDeclSyntax? {
+    public var setter: AccessorDeclSyntax? {
         get {
             if case let .accessors(list) = accessor {
                 return list.accessors.first(where: {
@@ -58,7 +58,7 @@ extension PatternBindingSyntax {
         }
     }
 
-    var getter: AccessorDeclSyntax? {
+    public var getter: AccessorDeclSyntax? {
         get {
             switch accessor {
             case let .accessors(list):
@@ -107,7 +107,7 @@ extension PatternBindingSyntax {
         }
     }
 
-    var isGetOnly: Bool {
+    public var isGetOnly: Bool {
         if initializer != nil {
             return false
         }
